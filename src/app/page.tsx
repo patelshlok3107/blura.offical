0262ec57162f6db1e3ec1ade3e59e04382c566dc
patelshlok3107 <shlokpatel2599@gmail.com>
2026-05-20 11:55:22 +0530

    remove marquee section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '@/components/sections/HeroSection';
 import PourSection from '@/components/sections/PourSection';
-import MarqueeSection from '@/components/sections/MarqueeSection';
 import GlassSection from '@/components/sections/GlassSection';
 import FeaturesSection from '@/components/sections/FeaturesSection';
 import BulkInquirySection from '@/components/sections/BulkInquirySection';
@@ -14,7 +13,6 @@ export default function HomePage() {
     <>
       <HeroSection />
       <PourSection />
-      <MarqueeSection />
       <GlassSection />
       <FeaturesSection />
       <BulkInquirySection />
